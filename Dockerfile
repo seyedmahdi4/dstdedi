9a482f250d32b19a6bc508d092e3350bd39888bf
seyedmahdi4 <aghaseyed7@gmail.com>
2022-07-16 00:20:28 +0000

    fist try part 1

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM debian:latest
 RUN apt-get update && \
-    apt-get install lib32gcc-s1 wget unzip libcurl3-gnutls -y  && \
+    apt-get install lib32gcc-s1 wget unzip libcurl3-gnutls gosu -y  && \
+    rm -rf /var/lib/apt/lists/* && \
+    chmod +s /usr/sbin/gosu && \
     useradd -rm -d /home/dst -s /bin/bash dst
 USER dst
 WORKDIR /home/dst
 COPY --chown=dst:dst install.sh /home/dst/install.sh
 RUN chmod +x /home/dst/install.sh && /home/dst/install.sh
-COPY --chown=dst:dst example/ /home/dst/.klei/DoNotStarveTogether/example/
+COPY --chown=dst:dst example/ /home/dst/.klei/DoNotStarveTogether/example1/
 COPY --chown=dst:dst script.sh /home/dst/script.sh
 RUN  chmod +x /home/dst/script.sh
 CMD ["./script.sh"]
--- script.sh
+++ script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+gosu root chown dst:dst /home/dst/.klei/DoNotStarveTogether/example/
 PA="/home/dst/.klei/DoNotStarveTogether/example"
+cp -n -r  /home/dst/.klei/DoNotStarveTogether/example1/* /home/dst/.klei/DoNotStarveTogether/example/
 
 if [ -z "$MAX_PLAYER" ]; then
 MAX_PLAYER=6
@@ -7,8 +9,9 @@ fi
 echo "max player set $MAX_PLAYER"
 
 if [ -z "$CLUSTER_DESCRIPTION" ]; then
-CLUSTER_DESCRIPTION="Powered by Docker - seyedmahdi3/dstdedi"
+CLUSTER_DESCRIPTION="Powered by Docker - seyedmahdi3\/dstdedi"
 fi
+echo -e "description is $CLUSTER_DESCRIPTION"
 
 if [ -z "$CLUSTER_NAME" ]; then
 CLUSTER_NAME="dstdedi -- test"
@@ -20,20 +23,65 @@ echo ERROR 404! CLUSTER_TOKEN not found
 exit 1
 fi
 
+if [ -z "$GAMEMODE" ]; then
+GAMEMODE="endless"
+fi
+echo "gamemode is : $GAMEMODE"
+
+if [ -z "$STYLE" ]; then
+STYLE="cooperative"
+fi
+echo "playstyle is : $STYLE"
+
+if [ -z "$PVP" ]; then
+PVP="false"
+fi
+echo "PVP is : $PVP"
+
+
+if [ -z "$AUTOPAUSE" ]; then
+AUTOPAUSE="true"
+fi
+echo "autopause is : $AUTOPAUSE"
+
+
 sed -i "s/cluster_name = .*$/cluster_name = ${CLUSTER_NAME}/g" $PA/cluster.ini
 sed -i "s/cluster_description = .*$/cluster_description = ${CLUSTER_DESCRIPTION}/g" $PA/cluster.ini
 sed -i "s/max_players = .*$/max_players = ${MAX_PLAYER}/g" $PA/cluster.ini
 sed -i "s/cluster_password = .*$/cluster_password = ${CLUSTER_PASSWORD}/g" $PA/cluster.ini
+
+sed -i "s/pvp = .*$/pvp = ${PVP}/g" $PA/cluster.ini
+sed -i "s/cluster_intention = .*$/cluster_intention = ${STYLE}/g" $PA/cluster.ini
+sed -i "s/pause_when_empty = .*$/pause_when_empty = ${AUTOPAUSE}/g" $PA/cluster.ini
+sed -i "s/game_mode = .*$/game_mode = ${GAMEMODE}/g" $PA/cluster.ini
 echo $CLUSTER_TOKEN > $PA/cluster_token.txt
-echo $ADMIN_ID > $PA/adminlist.txt
 
-#sample
-if [ "$HALLOWED" = TRUE  ]; then
-echo -e "ServerModSetup(\"2633870801\")"   >> ./dontstarvetogether_dedicated_server/mods/dedicated_server_mods_setup.lua
-echo "hallowed forge ON"
+IFS=',' read -ra admins <<< "${ADMIN_IDS}"
+for admin in "${admins[@]}"; do
+  echo $admin >> $PA/adminlist.txt
+  echo "admin $admin added"
+done
+
+
+#mod
+
+if [ ! -z "$MODS" ]; then
+if [ ! -f $PA/Master/modoverrides.lua  ]; then
+MODOVER="$PA/Master/modoverrides.lua"
+MODOVER_Caves="$PA/Caves/modoverrides.lua"
+echo -e "return {" > $MODOVER
+IFS=',' read -ra all_mods <<< "${MODS}"
+for mod in "${all_mods[@]}"; do
+  echo -e "[\"workshop-${mod}\"]={enabled=true}," >> $MODOVER
+  echo -e "[\"workshop-${mod}\"]={enabled=true}," >> $MODOVER_Caves
+  echo -e "ServerModSetup(\"${mod}\")"   >> ./dontstarvetogether_dedicated_server/mods/dedicated_server_mods_setup.lua
+  echo  "Mod ${mod} is enable"
+done
+echo -e "}" >> $MODOVER
 fi
-
+fi
 
 cd /home/dst/dontstarvetogether_dedicated_server/bin64
 echo "running server . . ."
-./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster example -monitor_parent_process $$ -shard Master
+./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster example -monitor_parent_process $$ -shard Master & \
+./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster example -monitor_parent_process $$ -shard Caves
