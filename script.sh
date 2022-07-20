b6b3638b3f4991fbc21e4cc02f065fff13437238
seyedmahdi4 <aghaseyed7@gmail.com>
2022-07-20 15:39:27 +0000

    bugfix

--- script.sh
+++ script.sh
@@ -34,6 +34,15 @@ cp -n -r  /home/dst/.klei/DoNotStarveTogether/example_world/* $PA
 fi
 fi
 
+if [ -z "$CLUSTER_TOKEN" ] || [ "$CLUSTER_TOKEN" = "****"  ] ; then
+echo ERROR 404! CLUSTER_TOKEN not found
+exit 1
+fi
+
+if [ -z "$CLUSTER_NAME" ]; then
+CLUSTER_NAME="dstdedi -- test"
+fi
+echo "your server name is : $CLUSTER_NAME"
 
 if [ -z "$MAX_PLAYER" ]; then
 MAX_PLAYER=6
@@ -45,15 +54,6 @@ CLUSTER_DESCRIPTION="Powered by Docker - seyedmahdi3\/dstdedi"
 fi
 echo -e "description set to: $CLUSTER_DESCRIPTION"
 
-if [ -z "$CLUSTER_NAME" ]; then
-CLUSTER_NAME="dstdedi -- test"
-fi
-echo "your server name is : $CLUSTER_NAME"
-
-if [ -z "$CLUSTER_TOKEN" ]; then
-echo ERROR 404! CLUSTER_TOKEN not found
-exit 1
-fi
 
 
 if [ -z "$GAMEMODE" ]; then
@@ -80,8 +80,9 @@ if [ -z "$AUTOPAUSE" ]; then
 AUTOPAUSE="true"
 fi
 echo "autopause is : $AUTOPAUSE"
-
-
+if [ ! -z "$CLUSTER_PASSWORD" ]; then
+echo "password is set to: $CLUSTER_PASSWORD"
+fi
 sed -i "s/cluster_name = .*$/cluster_name = ${CLUSTER_NAME}/g" $PA/cluster.ini || fail "error in permision . make issues"
 sed -i "s/cluster_description = .*$/cluster_description = ${CLUSTER_DESCRIPTION}/g" $PA/cluster.ini
 sed -i "s/max_players = .*$/max_players = ${MAX_PLAYER}/g" $PA/cluster.ini
@@ -126,26 +127,26 @@ fi
 
 
 cd /home/dst/dontstarvetogether_dedicated_server/bin64
-if [ ! "$DEBUG" = true ]; then
+grep_cmd="grep 'Server registered\|is now ready\|Your Server Will Not Start\|No auth token could be found\|Account Failed\|Please visit\|Sim paused\|Client connected\|Client authenticated\|Announcement\|Say\|Sim unpaused\|request\:\|Spawning player at\|Server Autopaused\|ReceiveRemoteExecute\|Available disk space for save files'"
+if [ ! "$DEBUG" = "true" ]; then
 
 if [ "$REFORGED" = true ]; then
 echo "running Reforged server . . ."
-./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster world -monitor_parent_process $$ -shard Master |grep 'Server registered\|is now ready!'
+./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster world -monitor_parent_process $$ -shard Master |grep 'Server registered\|is now ready\|Your Server Will Not Start\|No auth token could be found\|Account Failed\|Please visit\|Sim paused\|Client connected\|Client authenticated\|Announcement\|Say\|Sim unpaused\|request\:\|Spawning player at\|Server Autopaused\|ReceiveRemoteExecute\|Available disk space for save files'
 else
 echo "running server . . ."
-./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster world -monitor_parent_process $$ -shard Master |grep 'Server registered\|is now ready!' & \
-./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster world -monitor_parent_process $$ -shard Caves |grep 'Server registered\|is now ready!'
+./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster world -monitor_parent_process $$ -shard Master |grep 'Server registered\|is now ready\|Your Server Will Not Start\|No auth token could be found\|Account Failed\|Please visit\|Sim paused\|Client connected\|Client authenticated\|Announcement\|Say\|Sim unpaused\|request\:\|Spawning player at\|Server Autopaused\|ReceiveRemoteExecute\|Available disk space for save files' & \
+./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster world -monitor_parent_process $$ -shard Caves | grep 'Server registered\|is now ready\|Your Server Will Not Start\|No auth token could be found\|Account Failed\|Please visit\|Sim paused\|Client connected\|Client authenticated\|Announcement\|Say\|Sim unpaused\|request\:\|Spawning player at\|Server Autopaused\|ReceiveRemoteExecute\|Available disk space for save files'
 fi
 
 else
 
-if [ "$REFORGED" = true ]; then
-echo "running Reforged server . . ."
+
+echo "running server . . ."
+if [ "$REFORGED" = true ] || [ "$CAVE" = false ]  ; then
 ./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster world -monitor_parent_process $$ -shard Master
 else
-echo "running server . . ."
 ./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster world -monitor_parent_process $$ -shard Master & \
 ./dontstarve_dedicated_server_nullrenderer_x64 -console -cluster world -monitor_parent_process $$ -shard Caves
 fi
-
 fi
