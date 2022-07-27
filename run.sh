49b038962c78b246ba3afe6cd6db400fe21e0398
seyedmahdi4 <aghaseyed7@gmail.com>
2022-07-27 12:33:46 +0000

    add SCREEN env and change path ih dockerfile

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-screen -dmS dst ./script.sh
-sleep 3
-tail -f .klei/DoNotStarveTogether/world/Master/server_chat_log.txt .klei/DoNotStarveTogether/world/Master/server_log.txt|grep -v "==> .klei"
+if [  "$SCREEN" = true ]; then
+  echo Screen mod is enable
+  screen -dmS dst -L server_runner
+  sleep 1
+  tail -f screenlog.0
+else
+server_runner
+fi
+
+
+#tail -f .klei/DoNotStarveTogether/world/Master/server_chat_log.txt .klei/DoNotStarveTogether/world/Master/server_log.txt|grep -v "==> .klei"
