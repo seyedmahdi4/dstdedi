81813f5912c5bf3595115e95debf8b5e6aeb53c8
aghaseyed1 <aghaseyed7@gmail.com>
2023-06-18 13:33:59 -0400

    bugfix and refactore code

--- example_Regorge/Master/modoverrides.lua
+++ example_Regorge/Master/modoverrides.lua
@@ -1,30 +1,19 @@
---[[
-Note: 
-kick - Enable/disable kick votes in lobby. Options: true (enabled), false (disabled)
-gamemode - Enable/disable game modes votes in lobby. Options: true (enabled), false (disabled)
-perks - Enable/disable ability to choose character's perks in lobby. Options: true (enabled), false (disabled)
-fixed_gamemode - Will disable game mode votes and your server will run only this game mode.
-Optons:
-nil - disabled
-"default" - default
-"hungry" - Hungry gnaw
-"darkness" - Pitch darkness
-"hard" - Hard game mode
-"scaling" - Scaling difficulty
-"endless" - Endless
-"no_sweat" - No sweat mode
-"thieves" - Shadow thieves
-"rush" - Rush game mode
-]]
-
 return {
-	["workshop-1916510390"] = {
-		configuration_options = {
-			kick = true,
-			gamemode = true,
-			perks = true,
-			fixed_gamemode = nil,
-		},
-		enabled = true
-	},
+  ["workshop-1918927570"]={
+    configuration_options={
+      [""]=0,
+      Gameplay=0,
+      Vote=0,
+      changablefemusic=true,
+      fixed_gamemode=false,
+      forceendmatch=false,
+      gamemode=true,
+      gamemodesign=false,
+      kick=true,
+      newsoil=false,
+      perks=true,
+      specialevents=false 
+    },
+    enabled=true 
+  } 
 }
